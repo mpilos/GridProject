85208efc0ed9e650b19ca218f2a48156463ed9eb
nompilo <nonnyenhle@gmail.com>
2018-07-12 14:57:40 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { swipeShouldReset } from 'ionic-angular/util/util';
+import { WinnerPage } from '../winner/winner';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+scoreTeamB=0;
+scoreTeamA=0;
+
   constructor(public navCtrl: NavController) {
 
   }
+  TeamB(value:number):void{
+    this.scoreTeamB+=value;}
 
+TeamA(value:number):void{
+  this.scoreTeamA+=value;
 }
+
+reset():void{
+  this.scoreTeamA=0;
+  this.scoreTeamB=0;
+}
+End():void{this.navCtrl.push(WinnerPage)}
+}
+
+
